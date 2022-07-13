5859ad7a85e4532874157461be94e533cf1507f2
Kartheek Palla <palla.kartheekreddy@gmail.com>
2022-07-13 11:57:24 +0530

    Issue #SB-000 fix: deploy issue fix

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,7 +1,7 @@
 'use strict'
 const env = process.env
 let envVariables = {
-  BASE_URL: env.inquiry_portal_base_url || 'dev.sunbirded.org',
+  BASE_URL: 'dev.sunbirded.org' || env.inquiry_portal_base_url,
   API_AUTH_TOKEN: env.inquiry_portal_api_auth_token || "",
   USER_API_TOKEN: env.inquiry_portal_user_api_token || "",
   PORTAL_COOKIES: "",
