c551c34bbd7bc1ed388c5c0b72a9edd89e8d4d05
Rajnish Dargan <rajnish.code30@gmail.com>
2023-01-06 12:48:43 +0530

    IQ-00 changing user name and github url

--- server/config/users.js
+++ server/config/users.js
@@ -2,16 +2,16 @@ module.exports = {
     CREATORS: [
         {
             id: '5a587cc1-e018-4859-a0a8-e842650b9d64',
-            fullName: 'N11',
-            firstName: 'N11',
+            fullName: 'Creator1',
+            firstName: 'Creator1',
             lastName: '',
             channelId: '01309282781705830427',
             frameworkId: 'inquiry_k-12'
         },
         {
             id: '0b71985d-fcb0-4018-ab14-83f10c3b0426',
-            fullName: 'N131',
-            firstName: 'N131',
+            fullName: 'Creator2',
+            firstName: 'Creator2',
             lastName: '',
             channelId: '01309282781705830427',
             frameworkId: 'inquiry_k-12'
@@ -20,8 +20,8 @@ module.exports = {
     REVIEWERS: [
         {
             id: 'ae94b68c-a535-4dce-8e7a-fb9662b0ad68',
-            fullName: 'N150',
-            firstName: 'N150',
+            fullName: 'Reviewer',
+            firstName: 'Reviewer',
             lastName: '',
             channelId: '01309282781705830427',
             frameworkId: 'ekstep_ncert_k-12'
